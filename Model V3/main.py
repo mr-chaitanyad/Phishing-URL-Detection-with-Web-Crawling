b49772b179ebca40ff427d6cd601f234e01f0d5b
mr-chaitanyad <chaitanyadhayarkar005@gmail.com>
2026-04-06 00:06:16 +0530

    deployment phase

--- Model V3/main.py
+++ Model V3/main.py
@@ -460,5 +460,7 @@ def features():
 if WsgiToAsgi is not None:
     asgi_app = WsgiToAsgi(app)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+app = app
